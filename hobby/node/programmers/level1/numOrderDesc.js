3da2712830d16476efbce77a5cb7bcb4bcef0f97
윤석규 <yskyu93@modutech.co.kr>
2019-04-03 09:16:45 +0900

    Added num order desc and search root

---
+++ hobby/node/programmers/level1/numOrderDesc.js
@@ -0,0 +1,23 @@
+function solution(n) {
+  const getArray = (n, acc = []) => {
+    if(n == 0) return acc;
+    acc.push(n % 10);
+    return getArray(Math.floor(n / 10), acc);
+  };
+
+  return Number(getArray(n).sort().reverse().join(''));
+}
+
+const testCase = [];
+testCase.push({
+  n: 118372,
+});
+testCase.push({
+  n: 123456789,
+});
+
+const runner = () => testCase.forEach(({n}) => {
+  console.log(solution(n));
+});
+
+module.exports = runner;
---
+++ hobby/node/programmers/level1/searchRoot.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  return Math.sqrt(n) !== Math.floor(Math.sqrt(n)) ? -1 : Math.pow(Math.sqrt(n) + 1, 2);
+}
+
+const testCase = [];
+testCase.push({
+  n: 121,
+});
+testCase.push({
+  n: 3,
+});
+
+const runner = () => testCase.forEach(({n}) => {
+  console.log(solution(n));
+});
+
+module.exports = runner;
