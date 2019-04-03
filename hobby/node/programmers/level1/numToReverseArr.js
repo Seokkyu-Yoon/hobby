10110b0172a986f4bcadcb2e553e65997b710381
윤석규 <yskyu93@modutech.co.kr>
2019-04-03 09:06:09 +0900

    Added num to reverse arr

---
+++ hobby/node/programmers/level1/numToReverseArr.js
@@ -0,0 +1,26 @@
+function solution(n) {
+  const getNumberToReverseArr = (n, acc = []) => {
+    if(n == 0) return acc;
+    acc.push(n % 10);
+    return getNumberToReverseArr(Math.floor(n/10), acc);
+  };
+
+  return getNumberToReverseArr(n);
+}
+
+const testCase = [];
+testCase.push({
+  n: 12345,
+});
+testCase.push({
+  n: 10000000000,
+});
+testCase.push({
+  n: 9876543210,
+});
+
+const runner = () => testCase.forEach(({n}) => {
+  console.log(solution(n));
+});
+
+module.exports = runner;
