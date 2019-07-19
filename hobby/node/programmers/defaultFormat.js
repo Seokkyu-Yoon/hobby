a5fcb757beec0528d76467d6236e0e2a8b9c1801
윤석규 <yskyu93@modutech.co.kr>
2019-07-19 11:26:19 +0900

    Modified [defaultFormat.js]

--- hobby/node/programmers/defaultFormat.js
+++ hobby/node/programmers/defaultFormat.js
@@ -8,8 +8,14 @@ testCase.push({
 
 });
 
-const runner = () => testCase.forEach(() => {
-  console.log(solution());
+const runner = () => testCase.forEach(({}, index) => {
+  console.log(`${index + 1}-case:`);
+  try {
+    const myResult = solution();
+    console.log(`  result: ${myResult}\tcorrect:${myResult}`);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = runner;
