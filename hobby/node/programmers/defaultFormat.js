d2bb5ab3f7a654909d90152d3da387bf7c084422
윤석규 <yskyu93@modutech.co.kr>
2019-09-04 18:37:57 +0900

    Modified [defaultFormat] select log to result

--- hobby/node/programmers/defaultFormat.js
+++ hobby/node/programmers/defaultFormat.js
@@ -12,10 +12,14 @@ const runner = () => testCase.forEach(({}, index) => {
   console.log(`${index + 1}-case:`);
   try {
     const myResult = solution();
-    console.log('  result');
-    console.log(myResult);
-    console.log('  correct');
-    console.log(myResult === answer);
+    // if answer is value use it
+    // console.log(`  result: ${myResult}\tcorrect:${myResult === result}`);
+
+    // if answer is list use it
+    // console.log('  result');
+    // console.log(myResult);
+    // console.log('  correct');
+    // console.log(myResult === answer);
   } catch (e) {
     console.log(e);
   }
